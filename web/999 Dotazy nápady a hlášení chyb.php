9c4333d88c77bc95eeb38d20ca4ef3eda6b0feda
Jaroslav Týc <mail@jaroslavtyc.com>
2020-09-22 13:06:19 +0200

    Přejmenováno několik duplicitních nadpisů

--- web/999 Dotazy nápady a hlášení chyb.php
+++ web/999 Dotazy nápady a hlášení chyb.php
@@ -12,7 +12,8 @@
     Skláníš svou hlavu před sochou s umě vyvedenými záhyby zlatobílého roucha. Jako každou noc se modlíš k Aurionovi,
     pánovi světla, aby daroval další den. Dnes je to ale jiné. Dnes necítíš odpověď. Dnes slunce nevyjde.
   </div>
-  <p id="debugContacts">Jestli vážně vidíš chybu, ať už pouhou chybějící čárku, nebo opravdový nesmysl, nahlaš nám to na
+  <p id="debugContacts" class="without-anchor-to-id">Jestli vážně vidíš chybu, ať už pouhou chybějící čárku, nebo
+    opravdový nesmysl, nahlaš nám to na
     <a href="https://discord.com/invite/x5DzAx3">Discordu</a>.</p>
   <p>Každá opravená chyba pomůže hráčům k lepšímu zážitku z Asterionu. Díky Ti.</p>
 </div>
