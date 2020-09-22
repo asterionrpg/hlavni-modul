3ed9e3af1a53031b43746f20beee4888e0a041c4
Jaroslav Týc <mail@jaroslavtyc.com>
2020-09-22 11:59:01 +0200

    Přidány na zkoušku kontakty na hlášení chyb

---
+++ web/999 Dotazy nápady a hlášení chyb.php
@@ -0,0 +1,23 @@
+<h2>Dotazy, odpovědi a hlášení chyb</h2>
+
+<h3>Dotazy a odpovědi</h3>
+<div>
+  Pokud si nejsi s něčím jistý, v hlavě ti hlodá otázka, na jazyk se ti dere "no jo, ale co když...", zeptej se na
+  <a href="https://discord.com/invite/x5DzAx3">Discordu</a>. Odpoví ti dlouholetí hráči, ba i samotní autoři!
+</div>
+
+<h3>Ha, chyba!</h3>
+<div>
+  <div class="introduction">
+    Skláníš svou hlavu před sochou s umě vyvedenými záhyby zlatobílého roucha. Jako každou noc se modlíš k Aurionovi,
+    pánovi světla, aby daroval další den. Dnes je to ale jiné. Dnes necítíš odpověď. Dnes slunce nevyjde.
+  </div>
+  <p id="debugContacts">Jestli vážně vidíš chybu, ať už pouhou chybějící čárku, nebo opravdový nesmysl, nahlaš nám to na
+    <a href="https://discord.com/invite/x5DzAx3">Discordu</a>.</p>
+  <p>Každá opravená chyba pomůže hráčům k lepšímu zážitku z Asterionu. Díky Ti.</p>
+</div>
+
+<h2>Konec</h2>
+<div>
+  A to je konec... Co? Kdepak, to je začátek. Vzhůru za dobrodružstvím!
+</div>
